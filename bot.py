6f3ecf0be6bf9388303a8d29a3e3f79b6c83ad23
Christopher Brasington <1186896+chrisbrasington@users.noreply.github.com>
2023-04-16 12:41:47 -0600

    steam incomplete

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@
 import json
 import asyncio
 import discord, requests
-import time, sys, io
+import time, sys, io, re
 from discord.ext import commands, tasks
 from bs4 import BeautifulSoup
 from selenium import webdriver
@@ -11,13 +11,18 @@ from selenium.webdriver.common.by import By
 
 # processed tweets
 processed_tweets = set()
+processed_steam = set()
 
 # first run
 first_run = True
 
-# configs for paring twitter/discord uers
-with open("config.json") as config_file:
-    config = json.load(config_file)
+# configs for paring twitter/discord users
+with open("config-twitter.json") as config_file:
+    twitter_config = json.load(config_file)
+
+# configs for paring steam/discord users
+with open("config-steam.json") as config_file:
+    steam_config = json.load(config_file)
 
 # Configure Discord bot
 bot = commands.Bot(
@@ -77,22 +82,112 @@ def get_tweets(username):
 
     return tweet_data
 
+def get_steam_uploads(username):
+
+    # with selenium, read from firefox headless
+    options = Options()
+    options.add_argument('-headless')
+    browser = webdriver.Firefox(options=options)
+    url = f'https://steamcommunity.com/id/{username}/screenshots/?appid=0&sort=newestfirst&browsefilter=myfiles&view=grid'
+    browser.get(url)
+    time.sleep(5)  # Add a 5-second wait for page load
+
+    # parse html
+    soup = BeautifulSoup(browser.page_source, 'html.parser')
+    
+    if not soup:
+        logging.error('Failed to create BeautifulSoup object')
+        return []
+
+    # filter page elements for array of tweets
+    profile_media_items = soup.find_all(attrs={'class': 'profile_media_item'})
+
+    for item in profile_media_items:
+
+        href = item.get('href')
+        print(href)
+
+        # Load the page from the href attribute
+        detail_page_response = browser.get(href)
+        time.sleep(5)
+        print(detail_page_response)
+        detail_page_soup = BeautifulSoup(detail_page_response.page_source, "html.parser")
+
+        # Find the element with the class 'actualmediactn' and get the href attribute of the child 'a' tag
+        actual_media_ctn = detail_page_soup.find(attrs={'class': 'actualmediactn'})
+        image_link = actual_media_ctn.find('a').get('href')
+
+        # print(detail_page_soup)
+        # print(actual_media_ctn)
+        # print(image_link)
+            
+
+
+
+
+        
+        ###################
+        # get IMAGE URL
+        # img_wall_item = item.find(attrs={'class': 'imgWallItem'})
+
+        # # Get the style attribute
+        # style = img_wall_item.get('style')
+
+        # # Extract the URL from the style attribute using a regular expression
+        # url_match = re.search(r"url\('(.+?)'\)", style)
+
+        # if url_match:
+        #     background_image_url = url_match.group(1)
+        #     background_image_url += '?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false'
+
+        #     print(background_image_url)
+
+        ###################
+        # get PUBLLISHED FILE ID FROM STEAM API
+        # published_file_id = item.get('data-publishedfileid')
+        # print(published_file_id)
+
+        # url = 'https://api.steampowered.com/ISteamRemoteStorage/GetPublishedFileDetails/v1/'
+
+        # params = {
+        #     'key': '783041FE166A0240714001AEBEBEDF7C',
+        #     'itemcount': 1,
+        #     'publishedfileids[0]': published_file_id
+        # }
+
+        # response = requests.post(url, data=params)
+        # response_data = response.json()
+
+        # print(response_data)
+
+
+
+        break
+
+
+
+    browser.quit()
+    return []
+
+    # return profile_media_items
+
 # post image to discord
-async def post_images(username, discord_user_id, channel_id):
+async def post_images(username, discord_user_id, channel_id, is_steam = False):
     global bot, first_run, processed_tweets
 
     # search for upload discord channel
     channel = bot.get_channel(channel_id)
-    if channel:
-        print(f'Channel found: {channel.name}')
-    else:
+    if not channel:
         print(f'Channel not found for ID: {channel_id}')
 
     # get tweets for single user (video skipped, not in resulting [])
-    tweets = get_tweets(username)
+    if is_steam:
+        posts = get_steam_uploads(username)
+    else:
+        posts = get_tweets(username)
 
     # for each tweet
-    for tweet in tweets:
+    for tweet in posts:
 
         # if first run, mark latest, do not re-upload to discord
         if first_run:
@@ -129,28 +224,42 @@ async def post_images(username, discord_user_id, channel_id):
                 else:
                     print('first run, setting latest..')
 
-    first_run = False
+    
 
 # check twitter on timer loop
 @tasks.loop(seconds=60)
 async def check_twitter():
-    global first_run
     print('checking twitter... ', end='')
 
     # for each user in config
-    for user in config["users"]:
+    for user in twitter_config["users"]:
         print(user)
         username = user["twitter_username"]
         discord_user_id = user["discord_user_id"]
-        channel_id = config['channel_id']
+        channel_id = twitter_config['channel_id']
 
-        await post_images(username, discord_user_id, channel_id)
+        await post_images(username, discord_user_id, channel_id, False)
+    print('done.')
+
+@tasks.loop(seconds=60)
+async def check_steam():
+    print('checking steam... ', end='')
+
+    # for each user in config
+    for user in steam_config["users"]:
+        print(user)
+        username = user["steam_username"]
+        discord_user_id = user["discord_user_id"]
+        channel_id = twitter_config['channel_id']
+
+        await post_images(username, discord_user_id, channel_id, True)
     print('done.')
 
 @bot.event
 async def on_ready():
     global first_run 
     print(f'Logged in as {bot.user.name}')
-    check_twitter.start()
+    # check_twitter.start()
+    check_steam.start()
 
-bot.run(config['discord_token'])
+bot.run(twitter_config['discord_token'])
